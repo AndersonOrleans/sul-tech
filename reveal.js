232a5c744c77a280f436479492b60c39c502ed79
Anderson Orleans <146341893+AndersonOrleans@users.noreply.github.com>
2024-11-12 22:56:38 -0300

    Colocando

---
+++ reveal.js
@@ -0,0 +1,79 @@
+
+window.revelar = ScrollReveal({reset:true})
+
+
+
+revelar.reveal('.efeito-js', 
+{
+    duration: 4000, 
+    distance: '90px'
+})
+
+revelar.reveal('.apresentação-p-js', {
+    duration: 12000,
+    distance: '90px',
+    origin: 'left',  // A animação do <p> virá da esquerda
+    reset: true
+});
+
+
+revelar.reveal('.especialidades-js', {
+    duration: 11000,
+    distance: '90px',
+    origin: 'left',  // A animação do <p> virá da esquerda
+    reset: true
+});
+
+
+revelar.reveal('.imagem-computador-js', {
+    duration: 13000,
+    distance: '90px',
+    origin: 'left',  // A animação do <p> virá da esquerda
+    reset: true
+});
+
+
+revelar.reveal('.separador-js', {
+    duration: 13000,
+    distance: '90px',
+    origin: 'left',  // A animação do <p> virá da esquerda
+    reset: true
+});
+
+
+revelar.reveal('.imagem-celular-js', {
+    duration: 12000,
+    distance: '90px',
+    origin: 'left',  // A animação do <p> virá da esquerda
+    reset: true
+});
+
+revelar.reveal('.contato-js', {
+    duration: 12000,
+    distance: '90px',
+    origin: 'left', // A animação do <p> virá da esquerda
+    
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
